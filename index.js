25b5c32224a790bfc19340585524f57c67af068e
larkinaa <104767715+SpicyBolas@users.noreply.github.com>
2024-02-25 15:23:44 +1100

    fix: debugged the date strings stored in db.

--- index.js
+++ index.js
@@ -73,51 +73,53 @@ app.get('/api/users',async function(req,res){
 //Create a POST for new exercises
 app.post('/api/users/:_id/exercises',async function(req,res){
   //If null date is input then assign today's date
-  if(req.body.date===null){
-    var date_input = new Date().toDateString();
+  if(typeof req.body.date === 'undefined'){
+    var date_input = new Date().toISOString().substring(0,10);
+    var date_output = new Date().toDateString();
+
   }else{
-    var date_input = new Date(req.body.date).toDateString();
+    var date_input = new Date(req.body.date).toISOString().substring(0,10);
+    var date_output = new Date(req.body.date).toDateString();
   }
+
   //Create new exercise entry
   var new_exercise = new Exercise({description: req.body.description, 
     date: date_input, duration: Number(req.body.duration), userID: req.params._id});
   await new_exercise.save();
 
   var user_details = await User.findById(req.params._id,'username _id').exec();
-  //Handle if username does not exist
-  if(user_details.username===null){
-    var output_username = '';
-  }else{
-    var output_username = user_details.username;
-  }
+  
   //TODO: make sure the date returned is 2 digits for the day
 
   //Return the response
   res.json({username: user_details.username,description: req.body.description,
-    duration: Number(req.body.duration), _id: user_details._id.toString(),date: date_input});
+    duration: Number(req.body.duration), _id: user_details._id.toString(),date: date_output});
 });
 
 //Create a GET response for logs given a user
-app.get('/api/users/:_id/logs?[from][&to][&limit]',async function(req,res){
+app.get('/api/users/:_id/logs',async function(req,res){
   var get_username = await User.findById(req.params._id,'username').exec();
   
   //Check for the existence of optional parameters from and to and perform the query in each case
   var query_exercises = Exercise.find({userID: req.params._id},'description duration date');
-
-  if(req.query.from!==null&req.query.to===null){
-    query_exercises = query_exercises.$where(`new Date(date)>=new Date(${req.query.from})`); 
-  }else if(req.query.from===null&req.query.to!==null){
-    query_exercises = query_exercises.$where(`new Date(date)<=new Date(${req.query.to})`);
-  }else if(req.query.from!==null&req.query.to!==null){
-    query_exercises.$where(`new Date(date)>=new Date(${req.query.from})&new Date(date)<=new Date(${req.query.to})`);
+  if(typeof req.query.from!=='undefined'&typeof req.query.to === 'undefined'){
+    query_exercises = query_exercises.find({date: {$gte: req.query.from}});
+  }else if(typeof req.query.from==='undefined'&typeof req.query.to !== 'undefined'){
+    query_exercises = query_exercises.find({date: {$lte: req.query.to}});
+  }else if(typeof req.query.from !== 'undefined'&typeof req.query.to !== 'undefined'){
+    query_exercises = query_exercises.find({date: {$gte: req.query.from,
+      $lte: req.query.to}});
   }
 
   //Check for existence of optional parameter limit and limit results if exists
-  if(req.query.limit!==null){
+  if(typeof req.query.limit!=='undefined'){
     query_exercises = query_exercises.limit(req.query.limit)
   }
   
   var get_exercises = await query_exercises.exec();
+  
+  get_exercises = get_exercises.map((elem)=>({description: elem.description,duration: elem.duration,date: new Date(elem.date).toDateString()}))
+
   var get_count = get_exercises.length;
   //Send logs as response
   res.json({username: get_username.username,count: get_count,_id: req.params._id,log: get_exercises})
